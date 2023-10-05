04044d4647b77b60090bfc33330c30ff2112f7d8
patteesolomon <patteesolomon@gmail.com>
2023-10-04 19:31:53 -0500

    render.yml

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,11 +4,12 @@ import dotenv from 'dotenv';
 const { Pool } = pkg;
 dotenv.config();
 const pool = new Pool({
-                user: process.env.PGUSER,
-                host: process.env.PGHOST,
-                password: process.env.PGPASSWORD,
-                database: process.env.PGDATABASE,
-                port: process.env.PGPORT,
+                user: process.env.USER,
+                host: process.env.HOST,
+                password: process.env.PASSWORD,
+                database: process.env.DATABASE,
+                port: process.env.PORT,
+                connectionString: process.env.CONNECTION_STRING
         })
 pool.connect();
 console.log('did we connex');
